f999c2e627cacc1159a90f4c32afb427d421798a
berrosnl <ber1803.lans@gmail.com>
2026-09-08 11:48:55 +0200

    mozaïek en opdracht

---
+++ Bernard Folder/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  strokeWeight(3)
+  rect(180, 125, 40, 140)
+  circle(200, 100, 110)
+  ellipse(170, 100, 30, 60)
+  ellipse(230, 100, 30, 60)
+  line(180, 265, 150, 300)
+  line(220, 265, 250, 300)
+  text("hellooo, its me, verity!")
+}
--- Week 1 - Hello World/sketch.js
+++ Week 1 - Hello World/sketch.js
@@ -1,7 +1,56 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(750, 500);
 }
 
 function draw() {
   background(220);
+  fill(0, 0, 0)
+  text("1. Bernard Lans", 50, 50);
+  text("2.", 50, 100);
+  fill(255, 0, 0);
+  strokeWeight(0);
+  rect(60, 100, 50, 100);
+  rect(160, 100, 50, 100);
+  fill(255);
+  rect(110, 100, 50, 100);
+  fill(0);
+  text("3.", 50, 250);
+  fill(255);
+  strokeWeight(3);
+  rect(70, 250, 90, 90);
+  fill(0);
+  strokeWeight(0)
+  rect(70, 250,30, 30 );
+  rect(130, 250,30, 30 );
+  rect(70, 310,30, 30 );
+  rect(100, 280,30, 30 );
+  rect(130, 310,30, 30 );
+  text("4.", 50, 380);
+  fill(220);
+  strokeWeight(3);
+  rect(75, 400, 80, 80);
+  line(70, 405, 115, 365);
+  line(160, 405, 115, 365);
+  fill(0);
+  text("5.", 220, 50);
+  fill(120);
+  strokeWeight(0);
+  rect(240, 50, 50, 150);
+  rect(255, 200, 20, 30);
+  fill(255, 0, 0);
+  circle(265, 80, 40);
+  fill(255, 165, 0);
+  circle(265, 130, 40);
+  fill(0, 128, 0);
+  circle(265, 175, 40);
+  fill(0);
+  text("6.", 220, 250);
+  fill(255);
+  strokeWeight(5);
+  rect(240, 260, 90, 90, 10);
+  fill(0);
+  strokeWeight(0);
+  circle(260, 280, 22);
+  circle(285, 305, 22);
+  circle(310, 330, 22);
 }
