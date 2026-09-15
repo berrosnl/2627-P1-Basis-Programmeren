114e5199c9fe68db63494597e46f9c08eb761154
berrosnl <ber1803.lans@gmail.com>
2026-09-15 13:51:46 +0200

    bomen

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,18 +1,23 @@
 // Zon variabelen
-let sun = 200;
-let sunspeed = 1;
+  let sun = 200;
+  let sunspeed = 1;
 
-let sunsize1 = 100;
+  let sunsize1 = 100;
 
 // Wolk variabelen
-let cloud1 = 900;
-let cloud1Speed = 1.5;
+  let cloud1 = 900;
+  let cloud1Speed = 1.5;
 
-let cloud2 = 900;
-let cloud2Speed = 1.05;
+  let cloud2 = 900;
+  let cloud2Speed = 1.05;
 
-let cloud3 = 900;
-let cloud3Speed = 1.2;
+  let cloud3 = 900;
+  let cloud3Speed = 1.2;
+
+// Boom variabelen
+  let boom = 0
+  let boom2 = 0
+
 
 function setup() {
   createCanvas(800, 600);
@@ -24,7 +29,7 @@ function draw() {
 // ZON
   fill(240, 170, 0, 150);
   circle(sun, 100, sunsize1);
-  sunsize1 = 100 + Math.sin(frameCount * 0.05) * 10
+  sunsize1 = 100 + Math.sin(frameCount * 0.05) * 10;
   
   
   fill(255, 255, 0);
@@ -45,42 +50,44 @@ function draw() {
   triangle(100, 550, 240, 300, 400, 550);
   triangle(900, 550, 600, 380, 400, 550);
   
+  
   // Wolken
     // Wolk 1
-  fill(200)
-  circle(890 - cloud1, 55, 50)
-  circle(930 - cloud1, 55, 65)
-  circle(970 - cloud1, 65, 50)
+  fill(200);
+  circle(890 - cloud1, 55, 50);
+  circle(930 - cloud1, 55, 65);
+  circle(970 - cloud1, 65, 50);
 
-  fill(255)
-  circle(890 - cloud1, 65, 50)
-  circle(930 - cloud1, 65, 65)
-  circle(970 - cloud1, 75, 50)
-  cloud1 = (frameCount * cloud1Speed % 1000)
+  fill(255);
+  circle(890 - cloud1, 65, 50);
+  circle(930 - cloud1, 65, 65);
+  circle(970 - cloud1, 75, 50);
+  cloud1 = (frameCount * cloud1Speed % 1000);
 
     // Wolk 2
-  fill(200)
-  circle(890 - cloud2, 185, 50)
-  circle(930 - cloud2, 185, 65)
-  circle(970 - cloud2, 195, 50)
+  fill(200);
+  circle(890 - cloud2, 185, 50);
+  circle(930 - cloud2, 185, 65);
+  circle(970 - cloud2, 195, 50);
 
-  fill(255)
-  circle(890 - cloud2, 195, 50)
-  circle(930 - cloud2, 195, 65)
-  circle(970 - cloud2, 205, 50)
-  cloud2 = (frameCount * cloud2Speed % 1000)
+  fill(255);
+  circle(890 - cloud2, 195, 50);
+  circle(930 - cloud2, 195, 65);
+  circle(970 - cloud2, 205, 50);
+  cloud2 = (frameCount * cloud2Speed % 1000);
 
     // Wolk 3
   fill(200)
-  circle(890 - cloud2, 125, 50)
-  circle(930 - cloud2, 125, 65)
-  circle(970 - cloud2, 135, 50)
+  circle(890 - cloud2, 125, 50);
+  circle(930 - cloud2, 125, 65);
+  circle(970 - cloud2, 135, 50);
 
   fill(255)
-  circle(890 - cloud2, 135, 50)
-  circle(930 - cloud2, 135, 65)
-  circle(970 - cloud2, 145, 50)
-  cloud2 = (frameCount * cloud3Speed % 1000)
+  circle(890 - cloud2, 135, 50);
+  circle(930 - cloud2, 135, 65);
+  circle(970 - cloud2, 145, 50);
+  cloud2 = (frameCount * cloud3Speed % 1000);
+  
   
   ///////
   //weg//
@@ -98,5 +105,93 @@ function draw() {
   rect(190, 555, 100, 10, 5);
   rect(370, 555, 100, 10, 5);
   rect(550, 555, 100, 10, 5);
+
+
+// Bomen
+  
+  // Boom 1
+    fill(137, 81, 42);
+    rect(50, 400, 20, 120);
+  
+    // Blaadjes
+  boom = 50 + Math .sin(frameCount * 0.1) * 5;
+    fill(11, 102, 35);
+    circle(boom, 400, 60);
+
+  boom = 50 + Math .sin(frameCount * 0.1) * 8;
+    fill(59, 177, 67);
+    circle(boom, 390, 60);
+
+  boom = 70 + Math .sin(frameCount * 0.1) * 5;
+    fill(11, 122, 35);
+    circle(boom, 400, 60);
+
+  boom = 70 + Math .sin(frameCount * 0.1) * 8;
+    fill(36, 169, 87);
+    circle(boom, 390, 60);
+
+  // Boom 2
+    fill(137, 81, 42);
+    rect(150, 400, 20, 120);
+  
+    // Blaadjes
+  boom = 150 + Math .sin(frameCount * 0.1) * 5;
+    fill(11, 102, 35);
+    circle(boom, 400, 60);
+
+  boom = 150 + Math .sin(frameCount * 0.1) * 8;
+    fill(59, 177, 67);
+    circle(boom, 390, 60);
+
+  boom = 170 + Math .sin(frameCount * 0.1) * 5;
+    fill(11, 122, 35);
+    circle(boom, 400, 60);
+
+  boom = 170 + Math .sin(frameCount * 0.1) * 8;
+    fill(36, 169, 87);
+    circle(boom, 390, 60);
+
+  // Boom 3
+    fill(137, 81, 42);
+    rect(280, 400, 20, 120);
+  
+    // Blaadjes
+  boom = 280 + Math .sin(frameCount * 0.1) * 5;
+    fill(11, 102, 35);
+    circle(boom, 400, 60);
+
+  boom = 280 + Math .sin(frameCount * 0.1) * 8;
+    fill(59, 177, 67);
+    circle(boom, 390, 60);
+
+  boom = 300 + Math .sin(frameCount * 0.1) * 5;
+    fill(11, 122, 35);
+    circle(boom, 400, 60);
+
+  boom = 300 + Math .sin(frameCount * 0.1) * 8;
+    fill(36, 169, 87);
+    circle(boom, 390, 60);
+
+  // Boom 4
+    fill(137, 81, 42);
+    rect(420, 400, 20, 120);
+  
+    // Blaadjes
+  boom = 420 + Math .sin(frameCount * 0.1) * 5;
+    fill(11, 102, 35);
+    circle(boom, 400, 60);
+
+  boom = 420 + Math .sin(frameCount * 0.1) * 8;
+    fill(42, 177, 67);
+    circle(boom, 390, 60);
+
+  boom = 440 + Math .sin(frameCount * 0.1) * 5;
+    fill(11, 122, 35);
+    circle(boom, 400, 60);
+
+  boom = 440 + Math .sin(frameCount * 0.1) * 8;
+    fill(36, 169, 87);
+    circle(boom, 390, 60);
+  
   
 }
